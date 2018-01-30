c684c1aa2b4be0e02ad10505fb247f0a5c5fa825
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2018-01-30 14:40:56 -0500

    creating new repo

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"os"
 	"strings"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
 )
 
 func main() {
@@ -13,13 +18,34 @@ func main() {
 		fail()
 	} else {
 		switch strings.ToLower(args[0]) {
+		case "new":
+			new(args[1])
 		case "help":
 			help()
 		default:
 			fail()
 		}
 	}
+}
 
+func new(name string) {
+	token := os.Getenv("GITHUB_AUTH_TOKEN")
+	if token == "" {
+		log.Fatal("No access token present")
+	}
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: string(token)})
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+	repo := &github.Repository{
+		Name: github.String(name),
+	}
+	_, _, err := client.Repositories.Create(ctx, "", repo)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("successfully created", name)
+	}
 }
 
 func fail() {
